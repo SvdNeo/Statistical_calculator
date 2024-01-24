f9bc933d19a4fa112728154c774d76c1b046851e
SvdNeo <sanjay.d@neoito.com>
2024-01-24 15:54:11 +0530

    created the method to get mean and median

---
+++ script.js
@@ -0,0 +1,10 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.slice().sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+}

07c3a35fca3e2c8a764566601d46662b53a2d14c
SvdNeo <sanjay.d@neoito.com>
2024-01-24 16:32:11 +0530

    created getMode function

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const getMedian = (array) => {
       : sorted[Math.floor(array.length / 2)];
   return median;
 }
+const getMode = (array) => {
+    const counts = {};
+    array.forEach((el) => {
+      counts[el] = (counts[el] || 0) + 1;
+    })
+    if (new Set(Object.values(counts)).size === 1) {
+      return null;
+    }
+    const highest = Object.keys(counts).sort(
+      (a, b) => counts[b] - counts[a]
+    )[0];
+    const mode = Object.keys(counts).filter(
+      (el) => counts[el] === counts[highest]
+    );
+    return mode.join(", ");
+  }
